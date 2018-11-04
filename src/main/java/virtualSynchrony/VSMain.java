3bf9a9d34c61fc727cb4816cc369827966a3f2a1
Danish Cheema <danishcheemapk@yahoo.com>
2018-11-04 23:03:18 +0100

    v1.03

--- src/main/java/virtualSynchrony/VSMain.java
+++ src/main/java/virtualSynchrony/VSMain.java
@@ -78,9 +78,9 @@ public class VSMain {
 	    	          system.dispatcher(), null
 	    	          );
 	    	
-	    	//this time must be grater than 1500ms as we are using 1500ms as flush timeout
+	    	//this time must be grater than 1000ms as we are using 1000ms as flush timeout plus 500ms for newP
 	    	system.scheduler().scheduleOnce(
-	    	          Duration.create(1900, TimeUnit.MILLISECONDS),  
+	    	          Duration.create(1950, TimeUnit.MILLISECONDS),  
 	    	          group.get(2),
 	    	          new StartChatMsg(sendStable), // the message to send
 	    	          system.dispatcher(), null
@@ -143,7 +143,7 @@ public class VSMain {
 	    	
 	    	sendStable=true;
 	    	system.scheduler().scheduleOnce(
-	    	          Duration.create(1550, TimeUnit.MILLISECONDS),  
+	    	          Duration.create(1950, TimeUnit.MILLISECONDS),  
 	    	          group.get(1),
 	    	          new StartChatMsg(sendStable), // the message to send
 	    	          system.dispatcher(), null
